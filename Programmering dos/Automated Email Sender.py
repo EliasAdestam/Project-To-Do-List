d140d309595a8cff967bd87fbcd1166f5cdf7452
Elias Adestam <eliade060327@student.jenseneducation.se>
2023-03-13 12:29:41 +0100

    Automatiserad Mail dorre

--- Programmering dos/Automated Email Sender.py
+++ Programmering dos/Automated Email Sender.py
@@ -1,7 +1,19 @@
 import smtplib
+import datetime
 
+#Mailserver samt portid
+smtp_mailInfo = smtplib.SMTP('smtp.gmail.com', 587)
 
-smtp_object = smtplib.SMTP('smtp.gmail.com', 587)
-smtp_object.starttls()
+#Krypterar mailet
+smtp_mailInfo.starttls()
 
-print("Welcome to the Automatic Email Sender by Goofball Studios")
+print("Welcome to the Automatic Email Sender by Goofballs dios")
+
+email = input("Enter your email: ")
+password = input("Enter your password: ")
+
+smtp_mailInfo.login(email, password)
+
+recipent = input("Enter the recipents email: ")
+subject = input("Enter the email subject: ")
+mainText = input("Enter the main text: ")
