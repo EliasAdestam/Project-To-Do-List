c96b0fd45bfe44b02fcd4e0ea6334bd5e47f6ee1
Elias Adestam <eliade060327@student.jenseneducation.se>
2023-03-29 09:25:38 +0200

    Uppdaterad to do list

--- Programmering dos/To-Do List.py
+++ Programmering dos/To-Do List.py
@@ -1,1 +1,5 @@
+import os
 print("Welcome to your to-do list! Select existing to-do list below or create a new one!")
+
+file = open("Todolist_1.txt", "a")
+file.write(input("Please enter your tasks here: "))
