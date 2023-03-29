225ef88a6dcbc64442dcd584b1fdd2672bd544f1
Elias Adestam <eliade060327@student.jenseneducation.se>
2023-03-29 09:32:16 +0200

    Ombadiidium

--- Programmering dos/To-Do List.py
+++ Programmering dos/To-Do List.py
@@ -1,5 +1,14 @@
 import os
-print("Welcome to your to-do list! Select existing to-do list below or create a new one!")
+
+folderPath = "C:/Users/EliasAdestam/PycharmProjects/Programmering dos"
+files = os.listdir(folderPath)
+txtFiles = [f for f in files if f.endswith('.txt')]
+
+print("Welcome to your to-do list! Select existing to-do list below or create a new one:")
+
+for txtFiles in txtFiles:
+    print(txtFiles)
+
 
 file = open("Todolist_1.txt", "a")
 file.write(input("Please enter your tasks here: "))
