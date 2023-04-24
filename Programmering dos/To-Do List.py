3ac8607e2f0d9a8455dfd6b3a9e14601ac302c86
Elias Adestam <eliade060327@student.jenseneducation.se>
2023-04-24 12:27:30 +0200

    24/4

--- Programmering dos/To-Do List.py
+++ Programmering dos/To-Do List.py
@@ -1,65 +1,69 @@
 import os
 
-def getTxtFilesInFolder(folder_path):
-    files = os.listdir(folder_path)
+def getTxtFilesInFolder(folderPath):                   #Visar .txt filerna i valda directory
+    files = os.listdir(folderPath)
     return [f for f in files if f.endswith('.txt') and f != "directoryPath.txt"]
 
-def getFilePath(folder_path, file_name):
-    return os.path.join(folder_path, f"{file_name}.txt")
+def getFilePath(folderPath, fileName):   #Hittar specifika filers platser i directory
+    return os.path.join(folderPath, f"{fileName}.txt")
 
-def readTasksFromFile(file_path):
-    with open(file_path, "r") as f:
+def readTasksFromFile(filePath):  #Öppnar och läser specifika filers innehåll
+    with open(filePath, "r") as f:
         tasks = f.readlines()
     return tasks
 
-def createOrOpenFile(file_path, create_file=False):
-    if os.path.exists(file_path):
+def createOrOpenFile(filePath, createFile=False):     #Ändrar existerande filer och skapar nya filer om den inte finns
+    if os.path.exists(filePath):
         mode = "a"
-    elif create_file:
+    elif createFile:
         mode = "w"
     else:
         return None
-    return open(file_path, mode)
+    return open(filePath, mode)
 
-def removeTask(file_path, task_index):
-    tasks = readTasksFromFile(file_path)
+def removeTask(filePath, taskIndex):              #Tar bort to-do grejer i själva To-Do-Listen
+    tasks = readTasksFromFile(filePath)
 
-    if task_index < 1 or task_index > len(tasks):
+    if taskIndex < 1 or taskIndex > len(tasks):
         print("Invalid task index.")
         return
 
-    task = tasks.pop(task_index - 1)
+    task = tasks.pop(taskIndex - 1)
 
-    with open(file_path, "w") as f:
+    with open(filePath, "w") as f:
         f.writelines(tasks)
 
     print(f"Task '{task.strip()}' removed from the file.")
 
 def main():
-    folder_path = input("Enter the directory where you want your lists to be saved here: ")
-    with open("directory_path.txt", "w") as f:
-        f.write(folder_path)
-    txt_files = getTxtFilesInFolder(folder_path)
+    folderPath = input("Enter the directory where you want your lists to be saved here: ")  #Här får användaren välja var filerna ska sparas
+    with open("directoryPath.txt", "w") as f:
+        f.write(folderPath)
 
-    print("Welcome to your to-do list! Select an existing to-do list below or create a new one:")
+    while not os.path.exists(folderPath):                                                 #En while-loop som ser till att programmet inte kraschar när katalogen inte hittas
+      folderPath = input("The directory could not be found. Please enter again: ")
+
+    txt_files = getTxtFilesInFolder(folderPath)
+
+    print("Welcome to your to-do list! Select an existing to-do list below or create a new one:")   #Här visar den existerande filer
 
     for txt_file in txt_files:
         print(txt_file[:-4])
 
-    file_name = input("Please enter the name of the file you want to edit or create: ")
-    file_path = getFilePath(folder_path, file_name)
+    fileName = input("Please enter the name of the file you want to edit or create: ")   #Här får användaren välja vilken fil den vill öppna eller skapa
+    filePath = getFilePath(folderPath, fileName)
 
-    file = createOrOpenFile(file_path, create_file=True)
+    file = createOrOpenFile(filePath, createFile=True)
     if file is None:
         print("Could not locate or create file. Exiting.")
         return
 
-    tasks = readTasksFromFile(file_path)
+    tasks = readTasksFromFile(filePath)        #Här visar den filerna i enumerate-läge vilket innebär att den inte kan ändras för tillfället
     print("Your current to-do list:")
     for i, task in enumerate(tasks):
         print(f"{i+1}. {task.strip()}")
 
-    while True:
+    while True:                 #Här är en while-loop som körs när filerna väl är öppna. Då kan man lägga till, ta bort och gå ur programmet.
         command = input("Enter 'add' to add a task, 'remove' to remove a task, or 'exit' to exit: ")
         if command.lower() == "exit":
             break
@@ -68,16 +72,16 @@ def main():
             file.write(task + "\n")
             print("Task added to the file.")
         elif command.lower() == "remove":
-            tasks = readTasksFromFile(file_path)
+            tasks = readTasksFromFile(filePath)
             print("Your current to-do list:")
             for i, task in enumerate(tasks):
                 print(f"{i+1}. {task.strip()}")
 
-            task_indices = [i+1 for i in range(len(tasks))]
-            print(f"Task indices: {task_indices}")
+            taskIndices = [i+1 for i in range(len(tasks))]
+            print(f"Task indices: {taskIndices}")
 
-            task_index = int(input(f"Please enter the index of the task you want to remove (1-{len(tasks)}): "))
-            removeTask(file_path, task_index)
+            taskIndex = int(input(f"Please enter the index of the task you want to remove (1-{len(tasks)}): "))
+            removeTask(filePath, taskIndex)
         else:
             print("Invalid command.")
 
